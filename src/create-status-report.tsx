56e22285e896a4f0c2d870137df7d7ed4baff386
Nabhag8848 <nabhagmotivaras76@gmail.com>
2024-02-24 15:38:36 +0530

    feat: create status report interface

--- src/create-status-report.tsx
+++ src/create-status-report.tsx
@@ -1,7 +1,39 @@
-import { Detail } from "@raycast/api";
+import { Action, ActionPanel, Form, Icon, PopToRootType, showHUD } from "@raycast/api";
 
 function CreateStatusReport() {
-  return <Detail markdown="# Hello World" />;
+  return (
+    <Form
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm
+            title="Create Report"
+            onSubmit={async (values) => {
+              console.log("onSubmit", values);
+              // Todo -> Make Network Request
+              await showHUD("Status Report Created 🎉", {
+                popToRootType: PopToRootType.Immediate,
+                clearRootSearch: true,
+              });
+            }}
+          />
+        </ActionPanel>
+      }
+    >
+      <Form.TextField id="title" title="Title" placeholder="Enter Title of your Outage..." autoFocus></Form.TextField>
+      <Form.Dropdown id="status" title="Status" placeholder="Select Current Status">
+        <Form.Dropdown.Item
+          title="Investigating"
+          value="investigating"
+          icon={Icon.MagnifyingGlass}
+        ></Form.Dropdown.Item>
+        <Form.Dropdown.Item title="Identified" value="identified" icon={Icon.Fingerprint}></Form.Dropdown.Item>
+        <Form.Dropdown.Item title="Monitoring" value="monitoring" icon={Icon.Heartbeat}></Form.Dropdown.Item>
+        <Form.Dropdown.Item title="Resolved" value="resolved" icon={Icon.Check}></Form.Dropdown.Item>
+      </Form.Dropdown>
+      <Form.DatePicker id="date" title="Date" defaultValue={new Date()} max={new Date()}></Form.DatePicker>
+      <Form.TextArea id="message" title="Message" placeholder="We are encountering..."></Form.TextArea>
+    </Form>
+  );
 }
 
 export default CreateStatusReport;
