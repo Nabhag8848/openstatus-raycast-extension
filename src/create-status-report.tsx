8a9135b1399fe964f2e5cbda22a0592b521213ba
Nabhag8848 <nabhagmotivaras76@gmail.com>
2024-02-26 13:48:58 +0530

    feat: validate report inputs and required fields

--- src/create-status-report.tsx
+++ src/create-status-report.tsx
@@ -1,29 +1,99 @@
 import { Action, ActionPanel, Form, Icon, PopToRootType, Toast, showHUD, showToast } from "@raycast/api";
+import { useState } from "react";
+
 import { openstatus } from "./services/OpenStatusSDK";
+import { Common } from "./enum/validation";
+
 import { StatusReport } from "./types/api";
+
 function CreateStatusReport() {
+  const [titleError, setTitleError] = useState<string | undefined>();
+  const [messageError, setMessageError] = useState<string | undefined>();
+
+  function isFormComplete(values: StatusReport) {
+    let result: boolean = true;
+    const { title, message } = values;
+
+    if (title.length === 0) {
+      setTitleError(Common.FORM_VALIDATION);
+      result = false;
+    }
+
+    if (message.length === 0) {
+      setMessageError(Common.FORM_VALIDATION);
+      result = false;
+    }
+
+    return result;
+  }
+
+  function validate(event) {
+    const { value, id } = event.target;
+
+    if (value && value.length > 0) {
+      switch (id) {
+        case "title": {
+          setTitleError(undefined);
+          break;
+        }
+        case "message": {
+          setMessageError(undefined);
+          break;
+        }
+      }
+
+      return;
+    }
+
+    switch (id) {
+      case "title": {
+        setTitleError(Common.FORM_VALIDATION);
+        break;
+      }
+      case "message": {
+        setMessageError(Common.FORM_VALIDATION);
+        break;
+      }
+    }
+  }
+
+  async function onSubmit(values: StatusReport) {
+    if (!isFormComplete(values)) {
+      return;
+    }
+
+    await showToast({
+      style: Toast.Style.Animated,
+      title: "Creating Status Report",
+    });
+    await openstatus.createStatusReport(values);
+    await showHUD("Status Report Created 🎉", {
+      popToRootType: PopToRootType.Immediate,
+      clearRootSearch: true,
+    });
+  }
+
   return (
     <Form
       actions={
         <ActionPanel>
-          <Action.SubmitForm
-            title="Create Report"
-            onSubmit={async (values: StatusReport) => {
-              await showToast({
-                style: Toast.Style.Animated,
-                title: "Creating Status Report",
-              });
-              await openstatus.createStatusReport(values);
-              await showHUD("Status Report Created 🎉", {
-                popToRootType: PopToRootType.Immediate,
-                clearRootSearch: true,
-              });
-            }}
-          />
+          <Action.SubmitForm title="Create Report" onSubmit={onSubmit} />
         </ActionPanel>
       }
     >
-      <Form.TextField id="title" title="Title" placeholder="Enter Title of your Outage..." autoFocus></Form.TextField>
+      <Form.TextField
+        id="title"
+        title="Title"
+        placeholder="Enter Title of your Outage..."
+        error={titleError}
+        onChange={(value) => {
+          if (value.length) {
+            setTitleError(undefined);
+          }
+        }}
+        onBlur={validate}
+        autoFocus
+      ></Form.TextField>
       <Form.Dropdown id="status" title="Status" placeholder="Select Current Status">
         <Form.Dropdown.Item
           title="Investigating"
@@ -34,8 +104,19 @@ function CreateStatusReport() {
         <Form.Dropdown.Item title="Monitoring" value="monitoring" icon={Icon.Heartbeat}></Form.Dropdown.Item>
         <Form.Dropdown.Item title="Resolved" value="resolved" icon={Icon.Check}></Form.Dropdown.Item>
       </Form.Dropdown>
+      <Form.TextArea
+        id="message"
+        title="Message"
+        placeholder="We are encountering..."
+        error={messageError}
+        onChange={(value) => {
+          if (value.length) {
+            setMessageError(undefined);
+          }
+        }}
+        onBlur={validate}
+      ></Form.TextArea>
       <Form.DatePicker id="date" title="Date" defaultValue={new Date()} max={new Date()}></Form.DatePicker>
-      <Form.TextArea id="message" title="Message" placeholder="We are encountering..."></Form.TextArea>
     </Form>
   );
 }
---
+++ src/enum/validation.ts
@@ -0,0 +1,3 @@
+export enum Common {
+  FORM_VALIDATION = "Required",
+}
